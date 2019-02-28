6dac31c4efea02542ad14f02464d1840e25c5ef1
basurohan <>
2019-02-28 11:37:31 -0500

    updated deployment file

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
 docker build -t basurohan/multi-client:latest -t basurohan/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t basurohan/multi-server:latest -t basurohan/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t basurohan/multi-worker:latest -t basurohan/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push basurohan/multi-client:latest
-docker push basurohan/multi-client:$SHA
 docker push basurohan/multi-server:latest
-docker push basurohan/multi-server$SHA
 docker push basurohan/multi-worker:latest
+
+docker push basurohan/multi-client:$SHA
+docker push basurohan/multi-server$SHA
 docker push basurohan/multi-worker:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=basurohan/multi-server:$SHA
 kubectl set image deployments/client-deployment client=basurohan/multi-client:$SHA
+kubectl set image deployments/server-deployment server=basurohan/multi-server:$SHA
 kubectl set image deployments/worker-deployment worker=basurohan/multi-worker:$SHA
